32a760752af7931af4fc3bd5a1e266ffc752deb9
Mariana Echeto <echetomariana@gmail.com>
2023-12-23 00:39:19 -0300

    fix

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,20 +3,34 @@ import urllib
 import requests
 import sys
 
-if len(sys.argv) > 1:
-    EMPLOYEE_ID = sys.argv[1]
-else:
-    exit
+"""_summary_
+"""
 
-URL = 'https://jsonplaceholder.typicode.com'
-EMPLOYEE = requests.get(f'{URL}/users/{EMPLOYEE_ID}').json()
-TOTAL_TASKS = requests.get(f'{URL}/todos?userId={EMPLOYEE_ID}').json()
-DONE_TASKS = requests.get(
-    f'{URL}/todos?userId={EMPLOYEE_ID}&completed=true').json()
-NUMBER_OF_TOTAL_TASKS = len(TOTAL_TASKS)
-NUMBER_OF_DONE_TASKS = len(DONE_TASKS)
 
-print(f'Employee {EMPLOYEE["name"]} is done with tasks' +
-      f'({NUMBER_OF_DONE_TASKS}/{NUMBER_OF_TOTAL_TASKS}):')
-for i in DONE_TASKS:
-    print(f'\t{i["title"]}')
+def getUserTasks(EMPLOYEE_ID):
+    """_summary_
+
+    Args:
+        EMPLOYEE_ID (_type_): _description_
+    """
+    URL = 'https://jsonplaceholder.typicode.com'
+    EMPLOYEE = requests.get(f'{URL}/users/{EMPLOYEE_ID}').json()
+    TOTAL_TASKS = requests.get(f'{URL}/todos?userId={EMPLOYEE_ID}').json()
+    DONE_TASKS = requests.get(
+        f'{URL}/todos?userId={EMPLOYEE_ID}&completed=true').json()
+    NUMBER_OF_TOTAL_TASKS = len(TOTAL_TASKS)
+    NUMBER_OF_DONE_TASKS = len(DONE_TASKS)
+
+    print(f'Employee {EMPLOYEE["name"]} is done with tasks' +
+          f'({NUMBER_OF_DONE_TASKS}/{NUMBER_OF_TOTAL_TASKS}):')
+    for i in DONE_TASKS:
+        print(f'\t{i["title"]}')
+
+
+if __name__ == "__main__":
+    """_summary_
+    """
+    if len(sys.argv) > 1:
+        getUserTasks(sys.argv[1])
+    else:
+        exit
